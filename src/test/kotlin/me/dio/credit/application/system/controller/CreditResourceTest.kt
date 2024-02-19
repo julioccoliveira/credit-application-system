2bf00703163758477df5e19888a0a45203dcc28c
Júlio Oliveira <juliocc_oliveira@hotmail.com>
2024-02-18 21:52:24 -0300

    Inicio dos testes no CreditResource

---
+++ src/test/kotlin/me/dio/credit/application/system/controller/CreditResourceTest.kt
@@ -0,0 +1,111 @@
+package me.dio.credit.application.system.controller
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import me.dio.credit.application.system.dto.request.CreditDto
+import me.dio.credit.application.system.dto.request.CustomerDto
+import me.dio.credit.application.system.dto.request.CustomerUpdateDto
+import me.dio.credit.application.system.entity.Credit
+import me.dio.credit.application.system.entity.Customer
+import me.dio.credit.application.system.repository.CreditRepository
+import me.dio.credit.application.system.repository.CustomerRepository
+import me.dio.credit.application.system.service.impl.CustomerService
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.http.MediaType
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.util.*
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@ContextConfiguration
+class CreditResourceTest {
+    @Autowired
+    private lateinit var creditRepository: CreditRepository
+    @Autowired
+    private lateinit var customerRepository: CustomerRepository
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+    @Autowired
+    private lateinit var objectMapper: ObjectMapper
+
+    companion object {
+        const val URL: String = "/api/credits"
+    }
+
+    @BeforeEach
+    fun setup() {
+        customerRepository.deleteAll()
+        creditRepository.deleteAll()
+    }
+
+    @AfterEach
+    fun tearDown() {
+        customerRepository.deleteAll()
+        creditRepository.deleteAll()
+    }
+
+    @Test
+    fun `should create a credit and return 201 status`() {
+        val customer: Customer = customerRepository.save(builderCustomerDto().toEntity())
+        val creditDto: CreditDto = builderCreditDto(customerId = customer.id!!)
+        val valueAsString: String = objectMapper.writeValueAsString(creditDto)
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.post(URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(valueAsString)
+        )
+            .andExpect(MockMvcResultMatchers.status().isCreated)
+
+    }
+
+    private fun builderCreditDto(
+        creditValue: BigDecimal = BigDecimal.valueOf(7500.0),
+        dayFirstOfInstallment: LocalDate = LocalDate.now().plusDays(30),
+        numberOfInstallments: Int = 6,
+        customerId: Long = 1L
+    ) = CreditDto(
+        creditValue = creditValue,
+        dayFirstOfInstallment = dayFirstOfInstallment,
+        numberOfInstallments = numberOfInstallments,
+        customerId = customerId
+    )
+
+    private fun builderCustomerDto(
+        firstName: String = "Cami",
+        lastName: String = "Cavalcante",
+        cpf: String = "28475934625",
+        email: String = "camila@email.com",
+        income: BigDecimal = BigDecimal.valueOf(1000.0),
+        password: String = "1234",
+        zipCode: String = "000000",
+        street: String = "Rua da Cami, 123",
+    ) = CustomerDto(
+        firstName = firstName,
+        lastName = lastName,
+        cpf = cpf,
+        email = email,
+        income = income,
+        password = password,
+        zipCode = zipCode,
+        street = street
+    )
+
+}
